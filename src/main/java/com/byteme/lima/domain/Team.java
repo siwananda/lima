eb4f807e58760984bcfdd7ebc357c0116230f99a
Jonathan Handoyo <jonathan.handoyo@gmail.com>
2016-02-20 22:49:20 +0800

    implementations for teams and disabling test for auto build

--- src/main/java/com/byteme/lima/domain/Team.java
+++ src/main/java/com/byteme/lima/domain/Team.java
@@ -1,10 +1,14 @@
 package com.byteme.lima.domain;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+@Getter
+@Setter
 @Document(collection = "teams")
 public class Team extends BaseDomain {
     @Transient
