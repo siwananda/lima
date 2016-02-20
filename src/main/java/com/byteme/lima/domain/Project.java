c30cc5b77c7bcd2c09f4ed0389aab904218daf3d
Hariyanto Tanudjaja <hariyanto.tanudjaja@gmail.com>
2016-02-21 00:10:37 +0800

    Add getter and setter

--- src/main/java/com/byteme/lima/domain/Project.java
+++ src/main/java/com/byteme/lima/domain/Project.java
@@ -1,11 +1,15 @@
 package com.byteme.lima.domain;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 import java.util.List;
 
+@Getter
+@Setter
 @Document(collection = "projects")
 public class Project extends BaseDomain {
     public Integer number;
