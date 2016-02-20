cb06e0c3dddebdce864ad9de9a1de7fa8432bd03
Wira Siwananda <siwananda@gmail.com>
2016-02-20 16:31:52 +0800

    init spring-boot

---
+++ src/main/java/com/byteme/lima/LimaApplication.java
@@ -0,0 +1,12 @@
+package com.byteme.lima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimaApplication.class, args);
+	}
+}
