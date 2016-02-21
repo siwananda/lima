98021d9dc12d5e4bcc627df4cb4fd424e218b933
Jonathan Handoyo <jonathan.handoyo@gmail.com>
2016-02-22 03:41:57 +0800

    disabling JUnit test

--- src/test/java/com/byteme/lima/JonathanTest.java
+++ src/test/java/com/byteme/lima/JonathanTest.java
@@ -1,38 +1,13 @@
 package com.byteme.lima;
 
-import com.byteme.lima.config.ApplicationConfiguration;
-import com.byteme.lima.domain.Project;
-import com.byteme.lima.domain.Task;
-import com.byteme.lima.domain.Team;
-import com.byteme.lima.domain.User;
-import com.byteme.lima.exception.IllegalStateException;
-import com.byteme.lima.service.EventService;
-import com.byteme.lima.service.ProjectService;
-import com.byteme.lima.service.TaskService;
-import com.byteme.lima.service.TeamService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.junit.runners.MethodSorters;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import javax.annotation.PostConstruct;
-
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(JUnit4.class)
 public class JonathanTest {
-
+    @Test
+    public void test1() {
+        assert true;
+    }
 }
